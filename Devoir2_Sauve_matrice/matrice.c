5f6c8dac8b9960acc128424b9f88ddeb907f7367
Ahmadoubambamb <mboup.ahmadou-bamba@ugb.edu.sn>
2024-11-21 03:02:58 +0000

    executable

--- Devoir2_Sauve_matrice/matrice.c
+++ Devoir2_Sauve_matrice/matrice.c
@@ -11,8 +11,11 @@ extern int optind;
 int main(int argc , char * argv[]){
     srand(time(NULL));
  int **matrix;
- int dim ,opt ,estbinaire = 0;
-  char nomfic[255] ;
+ int dim ,opt;
+ int estbinaire = 0;// pour savoir si le contenu a afficher dans l'option -a est
+                              //dans un fichier binaire ou text
+
+  char nomfic[255] ;// doit contenir le nom du fichier passer en argument
     while((opt = getopt(argc , argv , ":d:cabtf:")) != -1)
     {
         switch(opt)
@@ -23,46 +26,63 @@ int main(int argc , char * argv[]){
             case '?':
                 printf("option -%c inconnu \n",optopt);
                 break;
+
             case 'd':
                  dim = atoi(optarg);
                  printf("alloction d'une matrice de taille %d\n",dim);
+                 // on on alloue la matrice de dimension dim passer en argument dans l'option -d
                 matrix = (int **)malloc(dim * sizeof(int*));
                 for(int i = 0;i < dim;i++)
                     matrix[i] = (int*)malloc(dim * sizeof(int));
                 break;
+
             case 'c':
                 printf("creation d'une matrice aleatoire !\n");
                 for(int i =0 ;i < dim ;i++)
                     for(int j = 0;j < dim ; j++)
-                        matrix[i][j] = rand()%100 + 1;
+                        matrix[i][j] = rand()%100 + 1; //nombre aleatoire entre 0 et 100
                 break;
+
             case 'a':
-                if(estbinaire){
-                printf("affichage de la matrice :\n");
-                 int i = 0 , nbread = 0 , buf;
-                     int fdr = open(nomfic ,O_RDONLY);
-                     if(fdr == -1){
+                if(estbinaire) //affichage extrait dans le ficher binaire
+                {
+                printf("affichage de la matrice extrait dans le fichier binaire %s:\n",nomfic);
+                 int  nbread = 0 , buf;
+                     int fdrB = open(nomfic ,O_RDONLY);
+                     if(fdrB == -1){
                          perror("Ouverture pour lire");
                          return EXIT_FAILURE;
                     }
-                 do{ nbread = read(fdr, &buf, sizeof(int));
+                    int i = 0;
+                 do{ nbread = read(fdrB, &buf, sizeof(int));
                      i++;
                      if(nbread > 0)
                      {  printf("%5d ",buf);
+                         // si le rest de i div dim est null on va a la ligne pour afficher en format matrice
                       if(i%dim == 0)
                              printf("\n");
                      }
 
                    }while(nbread > 0);
-                 close(fdr);
+                 close(fdrB);
                 }
-                else{
-                    // text
+             else{ // extrait dans le ficher text pour afficher
+                printf("affichage de la matrice extrait dans le fichier text %s:\n",nomfic);
+                 int fdrT = open(nomfic , O_RDONLY);//Ouverture du fichier en mode lecture seul
+                 char Buf;
+                 while(read(fdrT ,&Buf ,sizeof(char)) > 0){
+                     printf("%c",Buf);
+                    }
+                    close(fdrT);
                 }
+
                         break;
+
             case 'f':
+                // on garde le nom du fichier passer en argument dans l'option -f
                 strcpy(nomfic,optarg);
                 printf("creation du fichier %s ",optarg);
+                // on cree le fichier uniquement sans l'Ouvrir
                 int fd = open(optarg, O_CREAT , S_IWUSR | S_IRUSR);
                 if(fd == -1){
                    printf("a echouer !!?\n");
@@ -74,7 +94,7 @@ int main(int argc , char * argv[]){
 
             case 'b':
                 estbinaire = 1;
-                 int fdwB = open(nomfic,O_WRONLY);
+                 int fdwB = open(nomfic,O_WRONLY); //Ouverture en en ecriture seul
                  if(fdwB == -1)
                  { perror("Ouverture !");
                      return EXIT_FAILURE;
@@ -83,12 +103,13 @@ int main(int argc , char * argv[]){
                 for(int i = 0;i < dim ;i++)
                     for(int j = 0;j < dim ;j++)
                          if(write(fdwB , &matrix[i][j] , sizeof(int)) <= 0){
-                             perror("Erreur d'ecritue dans le fichier ");
+                             perror("Erreur d'ecriture dans le fichier ");
                              return EXIT_FAILURE;
                         }
 
                 close(fdwB);
                 break;
+
             case 't':
                 estbinaire = 0;
                 int fdwT = open(nomfic ,O_WRONLY);
@@ -96,7 +117,7 @@ int main(int argc , char * argv[]){
                     fprintf(stderr,"erreur d'Ouverture");
                     return EXIT_FAILURE;
                 }
-               char enText[10] ;
+               char enText[10] ;// pour contenir l'entier en format text
                 for(int i = 0;i < dim ;i++){
                     for(int j = 0;j < dim ;j++){
                    int L = sprintf(enText,"%5d",matrix[i][j]);
