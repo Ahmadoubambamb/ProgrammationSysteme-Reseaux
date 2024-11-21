9d41a33583226b24e43700e00a3152177ee96564
Ahmadoubambamb <mboup.ahmadou-bamba@ugb.edu.sn>
2024-11-21 00:33:43 +0000

    ajout_matrice

--- Devoir2_Sauve_matrice/matrice.c
+++ Devoir2_Sauve_matrice/matrice.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
 #include  <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <string.h>
 #include <time.h>
 extern char * optarg;
 extern int optopt;
 extern int optind;
 int main(int argc , char * argv[]){
+    srand(time(NULL));
  int **matrix;
- int dim;
-    int opt ;
+ int dim ,opt ,estbinaire = 0;
+  char nomfic[255];
     while((opt = getopt(argc , argv , ":d:cabtf:")) != -1)
     {
         switch(opt)
@@ -33,12 +37,63 @@ int main(int argc , char * argv[]){
                         matrix[i][j] = rand()%100 + 1;
                 break;
             case 'a':
-                for(int i =0 ;i < dim ;i++)
-                 { for(int j = 0;j < dim ; j++)
-                        printf("%d\t",matrix[i][j]);
-                        printf("\n\n");}
+                if(estbinaire){
+                printf("affichage de la matrice :\n");
+                 int i = 0 , nbread = 0 , buf;
+                     int fdr = open(nomfic ,O_RDONLY);
+                     if(fdr == -1){
+                         perror("Ouverture pour lire");
+                         return EXIT_FAILURE;
+                    }
+                 do{ nbread = read(fdr, &buf, sizeof(int));
+                     i++;
+                     if(nbread > 0)
+                     {  printf("%5d ",buf);
+                      if(i%dim == 0)
+                             printf("\n");
+                     }
+
+                   }while(nbread > 0);
+                 close(fdr);
+                }
+                else{
+                    // text
+                }
                         break;
+            case 'f':
+                strcpy(nomfic,optarg);
+                printf("creation du fichier %s ",optarg);
+                int fd = open(optarg, O_CREAT , S_IWUSR | S_IRUSR);
+                if(fd == -1){
+                   printf("a echouer !!?\n");
+                    return EXIT_FAILURE;
+                }
+                printf("reussi avec succes !\n");
+                close(fd);
+                break;
+
+            case 'b':
+                estbinaire = 1;
+                 int fdw = open(nomfic,O_WRONLY);
+                 if(fdw == -1)
+                 { perror("Ouverture !");
+                     return EXIT_FAILURE;
+                 }
+
+                for(int i = 0;i < dim ;i++)
+                    for(int j = 0;j < dim ;j++)
+                         if(write(fdw , &matrix[i][j] , sizeof(int)) <= 0){
+                             perror("Erreur d'ecritue dans le fichier ");
+                             return EXIT_FAILURE;
+                        }
+
+                close(fdw);
+                break;
+            case 't':
+                estbinaire = 0;
+                for(int i = 0;i < dim ;i++)
+                    for(int j = 0;j < dim ;j++)
+                     //
         }
     }
-
 }
